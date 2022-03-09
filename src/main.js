dcabb86ab01e1cb9136cc467de65f981f6eda267
djawad <ghasedjavad@gmail.com>
2022-03-10 00:10:44 +0330

    add axios and interceptors

--- src/main.js
+++ src/main.js
@@ -11,5 +11,40 @@ app.use(router);
 
 
 
+import axios from 'axios';
+
+window.$http = axios.create({
+  baseURL: "",
+});
+
+$http.interceptors.request.use(
+  function(config){
+
+    const token = "a1b2c3d4";
+    config.headers['token'] = token;
+
+    return config;
+
+  },
+  function(error){
+    alert("خطا: "+error.request.data);
+    console.log(error, error.request.data);
+  }
+)
+
+$http.interceptors.response.use(
+  function(response){
+    return response;
+  },
+  function(error){
+    alert("خطا: "+error.response.data)
+    console.log(error, error.response.data)
+  }
+)
+
+
+
+
+
 
 app.mount('#app')
