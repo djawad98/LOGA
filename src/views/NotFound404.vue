11b0058836d1c11b463e7d21d49cd36e6cc6ceed
djawad <ghasedjavad@gmail.com>
2022-03-14 17:32:30 +0330

    chore

--- src/views/NotFound404.vue
+++ src/views/NotFound404.vue
@@ -2,7 +2,7 @@
     <div class="h-screen flex flex-col items-center justify-center">
         <p>
             صفحه موردنظر یافت نشد.
-            <router-link to="/" class="w-full" >بازگشت</router-link>
+            <router-link to="/" class="w-full underline">صفحه اصلی</router-link>
         </p>
     </div>
 </template>
